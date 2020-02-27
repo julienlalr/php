bdd1d0932112ab735f533a266fa4a1f83264b01a
Julien LALLIER <lallier@d17-l428-pc-17.ipa.ki.agh.edu.pl>
2020-02-27 08:54:29 +0100

    Add firt implementation of a program

--- SomeProgram.java
+++ SomeProgram.java
@@ -1,5 +1,6 @@
 public class SomeProgram {
 	public static void main(String [] args){
 		system.out.println("we are learning to use Git.");
+		system.out.println("We will be Git experts, soon.");
 		}
 }
