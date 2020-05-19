09abf712b7691318e016dabe79afde00316b7981
julienlalr <61537305+julienlalr@users.noreply.github.com>
2020-05-19 15:26:04 +0200

    Delete SomeProgram.java

--- SomeProgram.java
+++
@@ -1,6 +0,0 @@
-public class SomeProgram {
-	public static void main(String [] args){
-		system.out.println("we are learning to use Git.");
-		system.out.println("We will be Git experts, soon.");
-		}
-}
